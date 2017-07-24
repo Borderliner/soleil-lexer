9b67b0b345641d7bf18e02c2769bfe792eb7578a
Borderliner <hajianpour.mr@gmail.com>
2017-07-24 12:44:52 +0430

    Add InputSteam class

---
+++ src/input-stream.js
@@ -0,0 +1,54 @@
+class InputStream {
+  constructor(input) {
+    this.originalInput = input;
+    /** Unchangable originalInput
+     * NOTE: No need to return Object.freeze(). I just wanted to have a better
+     * programming style.
+     */
+    this.originalInput = Object.freeze(this.originalInput);
+    this.input = this.originalInput;
+    this.pos = 0;
+    this.line = 1;
+    this.col = 0;
+  }
+
+  /**
+   * Devours input and returns the next character from input.
+   * @return {Char} [Next character]
+   */
+  next() {
+    const char = this.input.charAt(this.pos + 1);
+    if (char === '\n') {
+      this.line += 1;
+      this.col = 0;
+    } else {
+      this.col += 1;
+    }
+    return char;
+  }
+
+  /**
+   * Returns the next character in input stream without devouring it.
+   * @return {Char} [Next character]
+   */
+  peek() {
+    return this.input.charAt(this.pos);
+  }
+
+  /**
+   * Returns true if the current character is EOF.
+   * @return {Boolean} EOF?
+   */
+  eof() {
+    return this.peek() === '';
+  }
+
+  /**
+   * Throws error message.
+   * @param  {String} message Error message
+   * @return {Void}         None
+   */
+  $error(message) {
+    throw new Error(`${message} => line: ${this.line}, column: ${this.col}`);
+  }
+}
